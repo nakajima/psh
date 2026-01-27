006827697354cbde37ae4f7bfc059ff68a61d37d
Pat Nakajima <patnakajima@gmail.com>
2026-01-27 14:15:33 -0800

    Initial Commit

---
+++ psh/pshApp.swift
@@ -0,0 +1,17 @@
+//
+//  pshApp.swift
+//  psh
+//
+//  Created by Pat Nakajima on 1/27/26.
+//
+
+import SwiftUI
+
+@main
+struct pshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pshTests/pshTests.swift
@@ -0,0 +1,16 @@
+//
+//  pshTests.swift
+//  pshTests
+//
+//  Created by Pat Nakajima on 1/27/26.
+//
+
+import Testing
+
+struct pshTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
